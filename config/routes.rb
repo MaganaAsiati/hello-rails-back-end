710f836f9c4d9edb40934e1fe26607e0ab2f0a46
MaganaAsiati <assia.magana@gmail.com>
2022-09-29 14:26:11 +0200

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :greetings     
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '', to: 'static#index', constraints: lambda { |req|
+  !req.xhr? && req.format.html?
+}
+  root "static#index"
 end
